dac43ef0ec2e065c5b8c604094a20d3525a65415
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2024-11-06 04:37:08 +0700

    films serverA call serverB

--- Homework/api-security/serverA/src/films/films.controller.ts
+++ Homework/api-security/serverA/src/films/films.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Ip, Inject, LoggerService } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Ip, Inject, LoggerService,
+   HttpException, HttpStatus
+ } from '@nestjs/common';
+ import { HttpService } from '@nestjs/axios';
+ import { firstValueFrom } from 'rxjs';
+
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import { FilmsService } from './films.service';
+// import { FilmsService } from './films.service';
 import { CreateFilmDto } from './dto/create-film.dto';
 import { UpdateFilmDto } from './dto/update-film.dto';
 
@@ -11,7 +16,8 @@ import { UpdateFilmDto } from './dto/update-film.dto';
 @Controller('films')
 export class FilmsController {
   constructor(
-    private readonly filmsService: FilmsService,
+    private readonly httpService: HttpService,
+    // private readonly filmsService: FilmsService,
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService
   ) {}
 
@@ -19,55 +25,56 @@ export class FilmsController {
   @ApiResponse({ status: 400, description: 'Bad Request', example: { title: 'Film Title' }  })
   @Post()
   async create(@Ip() ip: string, @Body() createFilmDto: CreateFilmDto) {
-    const data = await this.filmsService.create(createFilmDto);
+    // const data = await this.filmsService.create(createFilmDto);
+    const response = await firstValueFrom(this.httpService.post('http://localhost:5000/api/films', createFilmDto));
 
     this.logger.log(`Request: POST /api/films - Body: ${JSON.stringify(createFilmDto)} - ${ip}`, FilmsController.name);
-    this.logger.log(`Response: POST /api/films - Body: ${JSON.stringify(data)} - ${ip}`, FilmsController.name);
+    this.logger.log(`Response: POST /api/films - Body: ${JSON.stringify(response.data)} - ${ip}`, FilmsController.name);
 
-    return data;
+    return response.data;
   }
 
   @ApiResponse({ status: 200, description: 'Success' })
   @Get()
   async findAll(@Ip() ip: string) {
-    const data = await this.filmsService.findAll();
+    const response = await firstValueFrom(this.httpService.get('http://localhost:5000/api/films'));
 
     this.logger.log(`Request: GET /api/films - ${ip}`, FilmsController.name);
-    this.logger.log(`Response: GET /api/films - Body: ${JSON.stringify(data)} - ${ip}`, FilmsController.name);
+    this.logger.log(`Response: GET /api/films - Body: ${JSON.stringify(response.data)} - ${ip}`, FilmsController.name);
 
-    return data;
+    return response.data;
   }
 
   @ApiResponse({ status: 200, description: 'Success' })
   @Get(':id')
   async findOne(@Ip() ip: string, @Param('id', ParseIntPipe) id: number) {
-    const data = await this.filmsService.findOne(id);
+    const response = await firstValueFrom(this.httpService.get(`http://localhost:5000/api/films/${id}`));
 
     this.logger.log(`Request: GET /api/films/${id} - ${ip}`, FilmsController.name);
-    this.logger.log(`Response: GET /api/films/${id} - Body: ${JSON.stringify(data)} - ${ip}`, FilmsController.name);
+    this.logger.log(`Response: GET /api/films/${id} - Body: ${JSON.stringify(response.data)} - ${ip}`, FilmsController.name);
 
-    return data;
+    return response.data;
   }
 
   @ApiResponse({ status: 200, description: 'Success', example: { title: 'Film Title' } })
   @Patch(':id')
-  update(@Ip() ip: string, @Param('id', ParseIntPipe) id: number, @Body() updateFilmDto: UpdateFilmDto) {
-    const data = this.filmsService.update(id, updateFilmDto);
+  async update(@Ip() ip: string, @Param('id', ParseIntPipe) id: number, @Body() updateFilmDto: UpdateFilmDto) {
+    const response = await firstValueFrom(this.httpService.patch(`http://localhost:5000/api/films/${id}`, updateFilmDto));
 
     this.logger.log(`Request: PATCH /api/films/${id} - Body: ${JSON.stringify(updateFilmDto)} - ${ip}`, FilmsController.name);
-    this.logger.log(`Response: PATCH /api/films/${id} - Body: ${JSON.stringify(data)} - ${ip}`, FilmsController.name);
+    this.logger.log(`Response: PATCH /api/films/${id} - Body: ${JSON.stringify(response.data)} - ${ip}`, FilmsController.name);
 
-    return data;
+    return response.data;
   }
 
   @ApiResponse({ status: 200, description: 'Success' })
   @Delete(':id')
-  remove(@Ip() ip: string, @Param('id', ParseIntPipe) id: number) {
-    const data = this.filmsService.remove(id);
+  async remove(@Ip() ip: string, @Param('id', ParseIntPipe) id: number) {
+    const response = await firstValueFrom(this.httpService.delete(`http://localhost:5000/api/films/${id}`));
 
     this.logger.log(`Request: DELETE /api/films/${id} - ${ip}`, FilmsController.name);
-    this.logger.log(`Response: DELETE /api/films/${id} - Body: ${JSON.stringify(data)} - ${ip}`, FilmsController.name);
+    this.logger.log(`Response: DELETE /api/films/${id} - Body: ${JSON.stringify(response.data)} - ${ip}`, FilmsController.name);
 
-    return data;
+    return response.data;
   }
 }
--- Homework/api-security/serverA/src/films/films.module.ts
+++ Homework/api-security/serverA/src/films/films.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { FilmsService } from './films.service';
 import { FilmsController } from './films.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HttpModule],
   controllers: [FilmsController],
-  providers: [FilmsService],
+  // providers: [FilmsService],
 })
 export class FilmsModule {}
