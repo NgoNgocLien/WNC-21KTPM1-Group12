28b01a9eab55caa3f411aefc03a9e2e722378c9e
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2024-12-15 13:05:54 +0700

    fix: conflict in prisma service

--- FinalProject/backend/src/modules/transactions/transactions.service.ts
+++ FinalProject/backend/src/modules/transactions/transactions.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 import { CreateTransactionDto } from './dto/createTransaction.dto';
 import { plainToClass } from 'class-transformer';
