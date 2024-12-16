14ad13ea83d04d0c70086f3c8b052ab866b13f73
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-12-16 23:10:11 +0700

    tailwind

--- FinalProject/frontend/src/App.js
+++ FinalProject/frontend/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
+import Header from './components/Header';
+
+function Layout() {
+  return (
+    <div className='layout'>
+      {/* <SideBar /> */}
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Routes >
+        <Route path="" element={<Layout />} >
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
 
-    </div>
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ FinalProject/frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <main>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </main>
+  )
+}
