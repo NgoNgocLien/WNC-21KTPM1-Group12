8d046a136966e77b88acb3373f7848984e1f8411
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-12-25 01:13:46 +0700

    feat: create debt ui

--- FinalProject/frontend/src/pages/SignIn/Customer/Debt/DebtList.jsx
+++ FinalProject/frontend/src/pages/SignIn/Customer/Debt/DebtList.jsx
@@ -3,15 +3,22 @@ import { useSelector, useDispatch } from "react-redux"
 import { fetchIncomingDebts, fetchOutgoingDebts } from "../../../../redux/debtThunk"
 import { IDLE } from "../../../../util/config"
 import DebtItem from "../../../../components/Debt/DebtItem"
+import { FaAddressBook } from "react-icons/fa"
+import ContactList from "../../../../components/Account/ContactList"
+import { fetchUserContacts } from "../../../../redux/userThunk"
 
 const INCOMING = 'INCOMING'
 const OUTGOING = 'OUTGOING'
 
 export default function DebtList() {
-  const [activeTab, setActiveTab] = useState(INCOMING)
-
-  const { incomingDebts, outgoingDebts, status } = useSelector((state) => state.debt)
   const dispatch = useDispatch()
+
+  const [activeTab, setActiveTab] = useState(INCOMING)
+  const [displayContacts, setDisplayContacts] = useState(false)
+  const [selectedContact, setSelectedContact] = useState(null);
+
+  const { account_number, balance, contacts, fullname } = useSelector((state) => state.user);
+  const { incomingDebts, outgoingDebts, status } = useSelector((state) => state.debt)
 
   useEffect(() => {
     if (status === IDLE) {
@@ -20,78 +27,154 @@ export default function DebtList() {
     }
   }, [dispatch, status]);
 
+  const handleClickContactBook = () => {
+    if (contacts === null) {
+      dispatch(fetchUserContacts());
+    }
+    setDisplayContacts(true);
+  }
+
   return (
-    <main className="mx-auto w-full max-w-4xl">
-      <h3 className="text-xl font-semibold mb-6">Danh sách nhắc nợ</h3>
-      <div className="rounded-2xl bg-white p-6">
-        <div className="rounded-xl bg-gray-100 p-1 flex items-center gap-1 text-md font-semibold">
-          <button
-            className={`flex-1 flex items-center justify-center gap-2 rounded-lg px-3 py-2  ${activeTab === INCOMING ? `text-red-800 bg-white` : `text-gray-500 hover:text-gray-700`}`}
-            onClick={() => setActiveTab(INCOMING)}
-          >
-            Nhắc nợ phải trả
-          </button>
-          <button
-            className={`flex-1 flex items-center justify-center gap-2 rounded-lg px-3 py-2 ${activeTab === OUTGOING ? `text-red-800 bg-white` : `text-gray-500 hover:text-gray-700`}`}
-            onClick={() => setActiveTab(OUTGOING)}
-          >
-            Nhắc nợ phải thu
-          </button>
+    <>
+      {
+        displayContacts && (
+          <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-20">
+            <ContactList isMutable={false} setSelectedContact={setSelectedContact} setDisplayContacts={setDisplayContacts} />
+          </div>
+        )
+      }
+
+      <main className="mx-auto w-full max-w-4xl">
+        <h3 className="text-xl font-semibold my-4">Tạo nhắc nợ</h3>
+        <div className="rounded-2xl bg-white p-6">
+          <form className="flex flex-col gap-4">
+            <div className="w-full flex justify-between items-center">
+              <label htmlFor="account-number" className="w-3/12 font-medium text-gray-900">
+                Đến số tài khoản
+              </label>
+              <div className="w-7/12 flex items-center">
+                <input
+                  id="account-number"
+                  name="account-number"
+                  type="text"
+                  required
+                  placeholder="Nhập số tài khoản"
+                  className="w-full rounded-xl bg-white px-3 py-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-800 text-md"
+                />
+                <FaAddressBook className="ms-2 size-6 text-red-800 cursor-pointer hover:text-red-700" onClick={handleClickContactBook} />
+              </div>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <label htmlFor="amount" className="w-3/12 font-medium text-gray-900">
+                Số tiền
+              </label>
+              <div className="w-7/12">
+                <input
+                  id="amount"
+                  name="amount"
+                  type="number"
+                  required
+                  placeholder="Nhập số tiền"
+                  className="w-full rounded-xl bg-white px-3 py-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-800 text-md"
+                />
+              </div>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <label htmlFor="message" className="w-3/12 font-medium text-gray-900">
+                Nội dung nhắc nợ
+              </label>
+              <div className="w-7/12">
+                <input
+                  id="message"
+                  name="message"
+                  type="text"
+                  required
+                  placeholder="Nhập nội dung nhắc nợ"
+                  className="w-full rounded-xl bg-white px-3 py-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-800 text-md"
+                />
+              </div>
+            </div>
+
+            {/* divider */}
+            <div className="w-full border-b border-gray-200 my-1"></div>
+            <button className="bg-red-800 text-white font-semibold rounded-xl py-2 px-4 self-end">
+              Tạo
+            </button>
+          </form>
         </div>
 
-        {activeTab === INCOMING && (
-          <>
-            {incomingDebts?.pending.length !== 0 && (
-              <>
-                <h4 className="text-md font-medium text-gray-500 mt-4">Đang chờ</h4>
-                <ul className="divide-y divide-gray-200">
-                  {incomingDebts?.pending.map((debt) =>
-                    <DebtItem key={debt.id} debt={debt} type={INCOMING} />
-                  )}
-                </ul>
-              </>
-            )}
+        <h3 className="text-xl font-semibold my-4">Danh sách nhắc nợ</h3>
+        <div className="rounded-2xl bg-white p-6">
+          <div className="rounded-xl bg-gray-100 p-1 flex items-center gap-1 text-md font-semibold">
+            <button
+              className={`flex-1 flex items-center justify-center gap-2 rounded-lg px-3 py-2  ${activeTab === INCOMING ? `text-red-800 bg-white` : `text-gray-500 hover:text-gray-700`}`}
+              onClick={() => setActiveTab(INCOMING)}
+            >
+              Nhắc nợ phải trả
+            </button>
+            <button
+              className={`flex-1 flex items-center justify-center gap-2 rounded-lg px-3 py-2 ${activeTab === OUTGOING ? `text-red-800 bg-white` : `text-gray-500 hover:text-gray-700`}`}
+              onClick={() => setActiveTab(OUTGOING)}
+            >
+              Nhắc nợ phải thu
+            </button>
+          </div>
 
-            {incomingDebts?.completed.length !== 0 && (
-              <>
-                <h4 className="text-md font-medium text-gray-500 mt-4">Đã hoàn tất</h4>
-                <ul className="divide-y divide-gray-200">
-                  {incomingDebts?.completed.map((debt) =>
-                    <DebtItem key={debt.id} debt={debt} type={INCOMING} />
-                  )}
-                </ul>
-              </>
-            )}
-          </>
-        )}
+          {activeTab === INCOMING && (
+            <>
+              {incomingDebts?.pending.length !== 0 && (
+                <>
+                  <h4 className="text-md font-medium text-gray-500 mt-4">Đang chờ</h4>
+                  <ul className="divide-y divide-gray-200">
+                    {incomingDebts?.pending.map((debt) =>
+                      <DebtItem key={debt.id} debt={debt} type={INCOMING} />
+                    )}
+                  </ul>
+                </>
+              )}
+
+              {incomingDebts?.completed.length !== 0 && (
+                <>
+                  <h4 className="text-md font-medium text-gray-500 mt-4">Đã hoàn tất</h4>
+                  <ul className="divide-y divide-gray-200">
+                    {incomingDebts?.completed.map((debt) =>
+                      <DebtItem key={debt.id} debt={debt} type={INCOMING} />
+                    )}
+                  </ul>
+                </>
+              )}
+            </>
+          )}
 
 
-        {activeTab === OUTGOING && (
-          <>
-            {outgoingDebts?.pending.length !== 0 && (
-              <>
-                <h4 className="text-md font-medium text-gray-500 mt-4">Đang chờ</h4>
-                <ul className="divide-y divide-gray-200">
-                  {outgoingDebts?.pending.map((debt) =>
-                    <DebtItem key={debt.id} debt={debt} type={OUTGOING} />
-                  )}
-                </ul>
-              </>
-            )}
+          {activeTab === OUTGOING && (
+            <>
+              {outgoingDebts?.pending.length !== 0 && (
+                <>
+                  <h4 className="text-md font-medium text-gray-500 mt-4">Đang chờ</h4>
+                  <ul className="divide-y divide-gray-200">
+                    {outgoingDebts?.pending.map((debt) =>
+                      <DebtItem key={debt.id} debt={debt} type={OUTGOING} />
+                    )}
+                  </ul>
+                </>
+              )}
 
-            {outgoingDebts?.completed.length !== 0 && (
-              <>
-                <h4 className="text-md font-medium text-gray-500 mt-4">Đã hoàn tất</h4>
-                <ul className="divide-y divide-gray-200">
-                  {outgoingDebts?.completed.map((debt) =>
-                    <DebtItem key={debt.id} debt={debt} type={OUTGOING} />
-                  )}
-                </ul>
-              </>
-            )}
-          </>
-        )}
-      </div>
-    </main>
+              {outgoingDebts?.completed.length !== 0 && (
+                <>
+                  <h4 className="text-md font-medium text-gray-500 mt-4">Đã hoàn tất</h4>
+                  <ul className="divide-y divide-gray-200">
+                    {outgoingDebts?.completed.map((debt) =>
+                      <DebtItem key={debt.id} debt={debt} type={OUTGOING} />
+                    )}
+                  </ul>
+                </>
+              )}
+            </>
+          )}
+        </div>
+      </main>
+
+    </>
   )
 }
