53d0832551b4c2334cfa6580ddfd2b1fc96d6efc
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-12-30 22:17:45 +0700

    update debt form

--- FinalProject/frontend/src/pages/SignIn/Customer/Debt/DebtList.jsx
+++ FinalProject/frontend/src/pages/SignIn/Customer/Debt/DebtList.jsx
@@ -6,7 +6,7 @@ import DebtItem from "../../../../components/Debt/DebtItem"
 import { FaAddressBook } from "react-icons/fa"
 import ContactList from "../../../../components/Account/ContactList"
 import { fetchUserContacts } from "../../../../redux/userThunk"
-import { Field, Formik, Form, ErrorMessage } from "formik"
+import { Formik, Form, ErrorMessage, useFormik } from "formik"
 import * as Yup from 'yup';
 
 const INCOMING = 'INCOMING'
@@ -41,12 +41,33 @@ export default function DebtList() {
     setDisplayContacts(true);
   }
 
-  useEffect(() => {
-    if (selectedContact) {
-      console.log(selectedContact);
-    }
-  }, [selectedContact]);
 
+  const formik = useFormik({
+    initialValues: {
+      account_number: null,
+      amount: null,
+      message: null
+    },
+    validationSchema: Yup.object({
+      account_number: Yup.string().required('Số tài khoản là bắt buộc'),
+      amount: Yup.number().required('Số tiền là bắt buộc'),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  })
+
+  useEffect(() => {
+    console.log(selectedContact)
+    if (selectedContact) {
+      formik.setFieldValue("account_number", selectedContact.account_number)
+    }
+  }, [selectedContact])
+
+  useEffect(() => {
+    formik.setFieldValue('message', `${fullname.toUpperCase()} nhắc trả tiền`);
+  }, [fullname])
 
   return (
     <>
@@ -61,71 +82,75 @@ export default function DebtList() {
       <main className="mx-auto w-full max-w-4xl">
         <h3 className="text-xl font-semibold my-4">Tạo nhắc nợ</h3>
         <div className="rounded-2xl bg-white p-6">
-          <Formik
-            initialValues={{
-              account_number: '',
-              amount: '',
-              message: ''
-            }}
-            validationSchema={createDebtSchema}
-            onSubmit={(values) => console.log(values)}
-          >
-            <Form className="flex flex-col gap-4">
-              <div className="w-full flex justify-between items-center">
-                <label htmlFor="account_number" className="w-3/12 font-medium text-gray-900">
-                  Đến số tài khoản
-                </label>
-                <div className="w-7/12 flex items-center rounded-xl outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-red-800">
-                  <Field
-                    id="account_number"
-                    name="account_number"
-                    value={selectedContact?.account_number || ''}
-                    type="text"
-                    required
-                    placeholder="Nhập số tài khoản"
-                    className="w-full flex-1 bg-white px-3 py-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 text-md"
-                  />
-                  <FaAddressBook className="focus-within:relative pr-3 size-8 text-gray-400 cursor-pointer hover:text-red-800" onClick={handleClickContactBook} />
-                </div>
+          <form className="flex flex-col" onSubmit={formik.handleSubmit}>
+            <div className="w-full flex justify-between items-center">
+              <label htmlFor="account_number" className="w-3/12 font-medium text-gray-900">
+                Đến số tài khoản
+              </label>
+              <div className="w-7/12 flex items-center rounded-xl outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-red-800">
+                <input
+                  id="account_number"
+                  name="account_number"
+                  onChange={formik.handleChange}
+                  value={formik.values.account_number}
+                  onBlur={formik.handleBlur}
+                  type="text"
+                  required
+                  placeholder="Nhập số tài khoản"
+                  className="w-full flex-1 bg-white px-3 py-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 text-md"
+                />
+                <FaAddressBook className="focus-within:relative pr-3 size-8 text-gray-400 cursor-pointer hover:text-red-800" onClick={handleClickContactBook} />
               </div>
-              <ErrorMessage name="account_number" component="div" className="text-red-800 text-end" />
-              <div className="w-full flex justify-between items-center">
-                <label htmlFor="amount" className="w-3/12 font-medium text-gray-900">
-                  Số tiền
-                </label>
-                <div className="w-7/12 flex items-center rounded-xl outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-red-800">
-                  <Field
-                    id="amount"
-                    name="amount"
-                    type="number"
-                    required
-                    placeholder="Nhập số tiền"
-                    className="w-full flex-1 bg-white px-3 py-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 text-md"
-                  />
-                  <label for="amount" class="focus-within:relative pr-3 text-md text-gray-400">VNĐ</label>
-                </div>
+            </div>
+            {formik.touched.account_number && formik.errors.account_number && (
+              <div className="w-7/12 ms-auto text-red-500 text-sm mt-1">{formik.errors.account_number}</div>
+            )}
+            <div className="w-full flex justify-between items-center mt-4">
+              <label htmlFor="amount" className="w-3/12 font-medium text-gray-900">
+                Số tiền
+              </label>
+              <div className="w-7/12 flex items-center rounded-xl outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-red-800">
+                <input
+                  id="amount"
+                  name="amount"
+                  type="number"
+                  value={formik.values.amount}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required
+                  placeholder="Nhập số tiền"
+                  className="w-full flex-1 bg-white px-3 py-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 text-md"
+                />
+                <label for="amount" class="focus-within:relative pr-3 text-md text-gray-400">VNĐ</label>
               </div>
-              <ErrorMessage name="amount" component="div" className="text-red-800 text-end" />
-              <div className="w-full flex justify-between items-center">
-                <label htmlFor="message" className="w-3/12 font-medium text-gray-900">
-                  Nội dung nhắc nợ
-                </label>
-                <div className="w-7/12">
-                  <Field
-                    id="message"
-                    name="message"
-                    type="text"
-                    placeholder="Nhập nội dung nhắc nợ"
-                    className="w-full rounded-xl bg-white px-3 py-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-800 text-md"
-                  />
-                </div>
+            </div>
+            {formik.touched.amount && formik.errors.amount &&
+              <div className="w-7/12 ms-auto text-red-500 text-sm mt-1">
+                {formik.errors.amount}
               </div>
-              <div className="w-full border-b border-gray-200 my-1"></div>
-              <button className="bg-red-800 text-white font-semibold rounded-xl py-2 px-4 self-end">
-                Tạo
-              </button>
-            </Form>
-          </Formik>
+            }
+            <div className="w-full flex justify-between items-center mt-4">
+              <label htmlFor="message" className="w-3/12 font-medium text-gray-900">
+                Nội dung nhắc nợ
+              </label>
+              <div className="w-7/12">
+                <input
+                  id="message"
+                  name="message"
+                  type="text"
+                  value={formik.values.message}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  placeholder="Nhập nội dung nhắc nợ"
+                  className="w-full rounded-xl bg-white px-3 py-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-800 text-md"
+                />
+              </div>
+            </div>
+            <div className="w-full border-b border-gray-200 my-6"></div>
+            <button className="bg-red-800 text-white font-semibold rounded-xl py-2 px-6 self-end" type="submit" disabled={formik.isSubmitting} onClick={formik.handleSubmit}>
+              Tạo
+            </button>
+          </form>
         </div>
 
         <h3 className="text-xl font-semibold my-4">Danh sách nhắc nợ</h3>
