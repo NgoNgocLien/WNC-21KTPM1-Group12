d4597208f0f4aad83c851a50276edae523256581
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-12-31 22:36:29 +0700

    feat: custom table manage customer

--- FinalProject/frontend/src/components/Table/CustomerTable.jsx
+++ FinalProject/frontend/src/components/Table/CustomerTable.jsx
@@ -1,60 +1,42 @@
-import React from "react";
+import React from 'react';
 import { FaEye } from 'react-icons/fa';
 
 const CustomerTable = ({ customers }) => {
   return (
-    <div className="overflow-x-auto rounded-lg border border-gray-200 shadow-md">
+    <div className="overflow-x-auto shadow-md">
       <table className="min-w-full divide-y divide-gray-200 bg-white">
-        <thead className="bg-gray-50">
+        <thead className="bg-red-100">
           <tr>
-            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-600">
-              ID
-            </th>
-            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-600">
-              Username
-            </th>
-            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-600">
-              Họ tên
-            </th>
-            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-600">
-              Email
-            </th>
-            <th className="py-3 px-4 text-center text-sm font-semibold text-gray-600">
-              Số điện thoại
-            </th>
-            <th className="py-3 px-4 text-center text-sm font-semibold text-gray-600">
-              Tài khoản thanh toán
-            </th>
-            <th className="py-3 px-4 text-center text-sm font-semibold text-gray-600">
-              Action
-            </th>
+            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">ID</th>
+            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Username</th>
+            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Họ tên</th>
+            <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Email</th>
+            <th className="py-3 px-4 text-center text-sm font-semibold text-gray-700">Số điện thoại</th>
+            <th className="py-3 px-4 text-center text-sm font-semibold text-gray-700">Tài khoản thanh toán</th>
+            <th className="py-3 px-4 text-center text-sm font-semibold text-gray-700">Action</th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200">
           {customers.map((customer, index) => (
             <tr
               key={customer.id}
-              className={index % 2 === 0 ? "bg-white" : "bg-red-50"}
+              className={index % 2 === 0 ? "bg-white" : "bg-white"}
             >
               <td className="py-3 px-4 text-center text-sm text-gray-700">{customer.id}</td>
-              <td className="py-3 px-4 text-sm text-gray-700">
-                {customer.username}
-              </td>
+              <td className="py-3 px-4 text-sm text-gray-700">{customer.username}</td>
               <td className="py-3 px-4 text-sm text-gray-700">{customer.fullname}</td>
               <td className="py-3 px-4 text-sm text-gray-700">{customer.email}</td>
               <td className="py-3 px-4 text-center text-sm text-gray-700">{customer.phone}</td>
               <td className="py-3 px-4 text-center text-sm text-gray-700">
                 {customer.accounts[0]?.account_number || 'Chưa có'}
               </td>
-              <td className="py-3 px-4 text-sm text-gray-700">
-                <div className="flex justify-center items-center">
-                  <button
-                    className="text-red-500 hover:text-red-700"
-                    title="View"
-                  >
-                    <FaEye className="w-4 h-4" />
-                  </button>
-                </div>
+              <td className="py-2 px-3 text-center text-sm text-gray-700">
+                <button
+                  className="text-red-500 hover:text-red-700 focus:outline-none transition duration-200 ease-in-out"
+                  title="View"
+                >
+                  <FaEye className="w-5 h-5" />
+                </button>
               </td>
             </tr>
           ))}
--- FinalProject/frontend/src/pages/SignIn/Employee/CustomerMgmt.jsx
+++ FinalProject/frontend/src/pages/SignIn/Employee/CustomerMgmt.jsx
@@ -3,11 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getCustomers } from '../../../redux/userThunk';
 import CustomerTable from '../../../components/Table/CustomerTable';
 import AddCustomerModal from '../../../components/CustomerMgmt/AddCustomerModal';
-import { PlusIcon } from '@heroicons/react/24/outline'; 
+import { PlusIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function CustomerMgmt() {
   const dispatch = useDispatch();
-  const { customers } = useSelector((state) => state.user)
+  const { customers } = useSelector((state) => state.user);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
@@ -25,27 +25,43 @@ export default function CustomerMgmt() {
   const sortedCustomers = customers ? [...customers].sort((a, b) => a.id - b.id) : [];
 
   return (
-    <div className="p-6 bg-white rounded-xl shadow-lg space-y-6">
-      <div className="flex items-center justify-between">
-        <p className="text-lg font-semibold text-gray-800">Quản lý khách hàng</p>
+    <div className="max-w-screen-xl mx-auto px-4 py-2">
+      <p className="text-xl font-semibold text-gray-800 mb-2">Quản lý khách hàng</p>
+      
+      <div className="flex justify-end mb-5">
         <button
           onClick={openModal}
-          className="py-2 px-4 bg-red-600 text-white rounded-xl shadow-md hover:bg-red-700 flex items-center space-x-2"
+          className="py-2 px-3 bg-red-600 text-white rounded-lg shadow-lg hover:bg-red-700 focus:outline-none flex items-center space-x-2 transition duration-300 ease-in-out"
         >
           <PlusIcon className="w-5 h-5" />
-          <span>Thêm khách hàng mới</span>
+          <span className="font-normal">Thêm khách hàng</span>
         </button>
       </div>
 
-      {customers ? (
-        <CustomerTable customers={sortedCustomers} />
-      ) : (
-        <p className="text-gray-600">Đang tải danh sách khách hàng...</p>
-      )}
+      <div className="p-4 bg-white rounded-xl shadow-lg space-y-6">
+        <div className="flex flex-col lg:flex-row items-center justify-between mb-6 mt-2">
+          <div className="relative w-full ">
+            <input
+              type="text"
+              placeholder="Tìm kiếm theo ..."
+              className="w-full p-1.5 px-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition duration-300 ease-in-out"
+            />
+            <button className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none">
+              <MagnifyingGlassIcon className="w-5 h-5" />
+            </button>
+          </div>
+        </div>
 
-      {isModalOpen && (
-        <AddCustomerModal isOpen={isModalOpen} closeModal={closeModal} />
-      )}
+        {customers ? (
+          <CustomerTable customers={sortedCustomers} />
+        ) : (
+          <p className="text-gray-600">Đang tải danh sách khách hàng...</p>
+        )}
+
+        {isModalOpen && (
+          <AddCustomerModal isOpen={isModalOpen} closeModal={closeModal} />
+        )}
+      </div>
     </div>
   );
 }
