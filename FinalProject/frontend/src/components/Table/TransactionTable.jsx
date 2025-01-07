c67a76ad7f1c17da398c030b9bbe12c702ad01c9
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2025-01-08 00:53:19 +0700

    feat: replace external history list by table

--- FinalProject/frontend/src/components/Table/TransactionTable.jsx
+++ FinalProject/frontend/src/components/Table/TransactionTable.jsx
@@ -3,31 +3,33 @@ import { format } from 'date-fns';
 
 const TransactionTable = ({ transactions, banks }) => {
   return (
-    <div className="overflow-x-auto shadow-md">
+    <div className="max-h-screen overflow-y-auto shadow-md">
       <table className="min-w-full divide-y divide-gray-200 bg-white">
         <thead className="bg-red-100">
         <tr>
           <th className="py-3 px-2 text-left text-sm font-semibold text-gray-700">Mã giao dịch</th>
-          <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Ngày giao dịch</th>
-          <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Tài khoản gửi</th>
-          <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Tài khoản nhận</th>
-          <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Ngân hàng</th>
-          <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Số tiền</th>
-          {/* <th className="py-3 px-4 text-left text-sm font-semibold text-gray-700">Nội dung</th> */}
+          <th className="py-3 px-2 text-left text-sm font-semibold text-gray-700">Ngày giao dịch</th>
+          <th className="py-3 px-2 text-left text-sm font-semibold text-gray-700">Tài khoản nội bộ</th>
+          <th className="py-3 px-2 text-left text-sm font-semibold text-gray-700">Ngân hàng liên kết</th>
+          <th className="py-3 px-2 text-left text-sm font-semibold text-gray-700">Số tiền</th>
+          <th className="py-3 pl-2 text-left text-sm font-semibold text-gray-700"></th>
         </tr>
       </thead>
-      <tbody className="divide-y divide-gray-200"y>
+      <tbody className="divide-y divide-gray-200">
         {transactions.length > 0 ? transactions.map((transaction, index) => (
           <tr key={index}>
-            <td className="py-3 px-2 text-left text-sm text-gray-700">{transaction.id}</td> 
-            <td className="py-3 px-4 text-left text-sm text-gray-700">
+            <td className="py-3 px-2 text-left text-sm text-gray-700 truncate max-w-72">{transaction.id}</td>
+            <td className="py-3 px-2 text-left text-sm text-gray-700">
               {transaction.transaction_time ? format(new Date(transaction.transaction_time), 'dd/MM/yyyy HH:mm:ss') : 'N/A'}
             </td>
-            <td className="py-3 px-4 text-left text-sm text-gray-700">{transaction.sender_account_number}</td>
-            <td className="py-3 px-4 text-left text-sm text-gray-700">{transaction.recipient_account_number}</td>
-            <td className="py-3 px-4 text-left text-sm text-gray-700">{banks[transaction.id_sender_bank === 1 ? transaction.id_recipient_bank : transaction.id_sender_bank]?.name}</td>
-            <td className="py-3 px-4 text-left text-sm text-gray-700">{new Intl.NumberFormat().format(transaction.transaction_amount)} VNĐ</td>
-            {/* <td className="py-3 px-4 text-left text-sm text-gray-700">{transaction.transaction_message}</td> */}
+            <td className="py-3 px-2 text-left text-sm text-gray-700">{transaction.id_sender_bank === 1 ? transaction.sender_account_number : transaction.recipient_account_number}</td>
+            <td className="py-3 px-2 text-left text-sm text-gray-700">{banks[transaction.id_sender_bank === 1 ? transaction.id_recipient_bank : transaction.id_sender_bank]?.name}</td>
+            <td className="py-3 px-2 text-left text-sm text-gray-700">{new Intl.NumberFormat().format(transaction.transaction_amount)} VNĐ</td>
+            <td className="py-3 px-2 text-left text-sm text-gray-700">
+              <span className={`px-2 py-1 rounded-lg ${transaction.id_sender_bank === 1 ? 'bg-yellow-500 text-white' : 'bg-green-500 text-white'}`}>
+                {transaction.id_sender_bank === 1 ? "Chuyển tiền" : "Nhận tiền"}
+              </span> 
+            </td>
           </tr>
         )) : (
           <tr>
